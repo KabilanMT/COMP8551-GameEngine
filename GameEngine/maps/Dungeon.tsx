d6869d369d0822818de43f7af8f50a9fccc8df30
William Bradshaw <williambradshaw604@gmail.com>
2020-10-30 12:29:35 -0700

    WIP test of map 13

---
+++ GameEngine/maps/Dungeon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2020.10.28" name="Dungeon" tilewidth="48" tileheight="48" tilecount="256" columns="16">
+ <image source="../../TempleAtonement/texture/fsm_Forest02_C.png" width="768" height="768"/>
+</tileset>
