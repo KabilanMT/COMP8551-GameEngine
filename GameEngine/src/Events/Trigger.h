f4bc6d6465dadd075c6d2d7a57746d6abb9803dd
Billy Wong <ocis_nicola@hotmail.com>
2020-10-30 17:41:32 -0700

    added trigger event

---
+++ GameEngine/src/Events/Trigger.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "entityx/entityx.h"
+
+struct Trigger : public Event<Trigger> {
+    Trigger(Entity gotTriggered, Entity triggeringEntity) : gotTriggered(gotTriggered), triggeringEntity(triggeringEntity), triggered(true), triggerEntered(true), triggerExited(false){}
+    Entity gotTriggered, triggeringEntity;
+    bool triggered, triggerEntered, triggerExited;  //triggered means trigger is currently firing;  triggerEntered means trigger has already fired before;  triggerExited means objects are no longer colliding;
+
+    bool triggerMatch(Entity otherGotTriggered, Entity otherTriggeringEntity){
+        return (otherGotTriggered == gotTriggered && otherTriggeringEntity == triggeringEntity);
+    }
+};
