0eb4fe47b343a7ce88ede25de6aa9a6bcaf879f4
JessicaDevitt <39601330+JessicaDevitt@users.noreply.github.com>
2020-11-10 00:38:37 -0800

    player image
    player image added to player script

--- GameEngine/maps/blobsprite_01.tsx
+++ GameEngine/maps/blobsprite_01.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="2020.10.28" name="blobsprite_01" tilewidth="29" tileheight="45" tilecount="1" columns="1">
- <image source="../../TempleAtonement/texture/blobsprite_01.png" width="29" height="45"/>
+ <image source="../../TempleAtonement/texture/monster_down.png" width="29" height="45"/>
 </tileset>
