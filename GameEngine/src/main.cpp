1760c443085e1f0f82199b94893749b3529db038
JessicaDevitt <39601330+JessicaDevitt@users.noreply.github.com>
2020-11-13 01:36:12 -0800

    rooms created
    rooms 5,6,7 created

--- GameEngine/src/main.cpp
+++ GameEngine/src/main.cpp
@@ -7,10 +7,11 @@ int main()
 {
     //create scene
     //Scene 2 is first for testing, scene 1 should be first
-    SceneManager::getInstance().addScene("Scene 2", "../TempleAtonement/src/maps/map02.tmx");
-    SceneManager::getInstance().addScene("Scene 1", "../TempleAtonement/src/maps/map01.tmx");
-    SceneManager::getInstance().addScene("Scene 3", "../TempleAtonement/src/maps/map03.tmx");
-    SceneManager::getInstance().addScene("Scene 4", "../TempleAtonement/src/maps/map04.tmx");
+    //SceneManager::getInstance().addScene("Scene 2", "../TempleAtonement/src/maps/map02.tmx");
+    //SceneManager::getInstance().addScene("Scene 1", "../TempleAtonement/src/maps/map01.tmx");
+    //SceneManager::getInstance().addScene("Scene 3", "../TempleAtonement/src/maps/map03.tmx");
+    //SceneManager::getInstance().addScene("Scene 4", "../TempleAtonement/src/maps/map04.tmx");
+   
     SceneManager::getInstance().addScene("Scene 5", "../TempleAtonement/src/maps/map05.tmx");
     SceneManager::getInstance().addScene("Scene 6", "../TempleAtonement/src/maps/map06.tmx");
     SceneManager::getInstance().addScene("Scene 7", "../TempleAtonement/src/maps/map07.tmx");
