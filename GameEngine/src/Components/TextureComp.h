53d31ccfbc8f632cb5f4cab6d9bd98e126a5e38b
Ian Younghusband <ianyounghusband@hotmail.com>
2020-11-17 15:26:23 -0800

    removed temp logs

--- GameEngine/src/Components/TextureComp.h
+++ GameEngine/src/Components/TextureComp.h
@@ -3,24 +3,19 @@
 
 struct TextureComp {
     TextureComp(const char* filepath): filepath(filepath) {
-        Logger::getInstance() << "TextureComp Constructor\n";
     }
 
     ~TextureComp() {
-        Logger::getInstance() << "TextureComp destructor\n";
         deInit();
     }
 
     void init() {
-        Logger::getInstance() << "TextureComp init\n";
         texture = new Texture(filepath);
         ready = true;
     }
 
     void deInit() {
-        Logger::getInstance() << "TextureComp deInit\n";
         if (texture != nullptr) {
-            Logger::getInstance() << "TextureComp before delete\n";
             delete texture;
             texture = nullptr;
         }
