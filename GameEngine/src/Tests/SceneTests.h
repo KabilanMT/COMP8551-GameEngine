719c6e54acd177d473db1dba190aae1f567a4e14
Darian Gratton <dariangratton@gmail.com>
2020-11-18 18:57:27 -0800

    Added tests for addCapsuleCollider, addCircleCollier, addCustomScript, and addRigidBody_2D

--- GameEngine/src/Tests/SceneTests.h
+++ GameEngine/src/Tests/SceneTests.h
@@ -224,3 +224,287 @@ TEST (AddCamera, MissingArguments) {
     EXPECT_EQ(camera.get()->y, 0);
     EXPECT_EQ(camera.get()->z, 0);
 }
+
+TEST (AddCapsuleCollider, ValidArguments) {
+    vector<string> parameters;
+    parameters.push_back("2");
+    parameters.push_back("45");
+    parameters.push_back("0");
+    parameters.push_back("0");
+    parameters.push_back("0");
+    parameters.push_back("0");
+    parameters.push_back("0");
+
+    Scene scene("Scene", "tmx");
+    Entity e = Engine::getInstance().entities.create();
+    scene.addCapsuleCollider(parameters, e);
+
+    EXPECT_TRUE(e.has_component<CapsuleCollider>());
+
+    ComponentHandle<CapsuleCollider> capCol = e.component<CapsuleCollider>();
+
+    EXPECT_EQ(capCol.get()->radius, 2);
+    EXPECT_EQ(capCol.get()->a, 45);
+    EXPECT_EQ(capCol.get()->x, 0);
+    EXPECT_EQ(capCol.get()->y, 0);
+    EXPECT_EQ(capCol.get()->z, 0);
+    EXPECT_FALSE(capCol.get()->isTrigger);
+    EXPECT_FALSE(capCol.get()->render);
+}
+
+TEST (AddCapsuleCollider, InvalidArguments) {
+    vector<string> parameters;
+    parameters.push_back("hello");
+    parameters.push_back("world");
+    parameters.push_back("a");
+    parameters.push_back("s");
+    parameters.push_back("d");
+    parameters.push_back("f");
+    parameters.push_back("g");
+
+    Scene scene("Scene", "tmx");
+    Entity e = Engine::getInstance().entities.create();
+    scene.addCapsuleCollider(parameters, e);
+
+    EXPECT_TRUE(e.has_component<CapsuleCollider>());
+
+    ComponentHandle<CapsuleCollider> capCol = e.component<CapsuleCollider>();
+
+    EXPECT_EQ(capCol.get()->radius, 1);
+    EXPECT_EQ(capCol.get()->a, 0);
+    EXPECT_EQ(capCol.get()->x, 0);
+    EXPECT_EQ(capCol.get()->y, 0);
+    EXPECT_EQ(capCol.get()->z, 0);
+    EXPECT_FALSE(capCol.get()->isTrigger);
+    EXPECT_FALSE(capCol.get()->render);
+}
+
+TEST (AddCapsuleCollider, MissingArguments) {
+    vector<string> parameters;
+
+    Scene scene("Scene", "tmx");
+    Entity e = Engine::getInstance().entities.create();
+    scene.addCapsuleCollider(parameters, e);
+
+    EXPECT_TRUE(e.has_component<CapsuleCollider>());
+
+    ComponentHandle<CapsuleCollider> capCol = e.component<CapsuleCollider>();
+
+    EXPECT_EQ(capCol.get()->radius, 1);
+    EXPECT_EQ(capCol.get()->a, 0);
+    EXPECT_EQ(capCol.get()->x, 0);
+    EXPECT_EQ(capCol.get()->y, 0);
+    EXPECT_EQ(capCol.get()->z, 0);
+    EXPECT_FALSE(capCol.get()->isTrigger);
+    EXPECT_FALSE(capCol.get()->render);
+}
+
+TEST (AddCircleCollider, ValidArguments) {
+    vector<string> parameters;
+    parameters.push_back("2");
+    parameters.push_back("0");
+    parameters.push_back("0");
+    parameters.push_back("0");
+    parameters.push_back("0");
+    parameters.push_back("0");
+
+    Scene scene("Scene", "tmx");
+    Entity e = Engine::getInstance().entities.create();
+    scene.addCircleCollider(parameters, e);
+
+    EXPECT_TRUE(e.has_component<CircleCollider>());
+
+    ComponentHandle<CircleCollider> cirCol = e.component<CircleCollider>();
+
+    EXPECT_EQ(cirCol.get()->radius, 2);
+    EXPECT_EQ(cirCol.get()->x, 0);
+    EXPECT_EQ(cirCol.get()->y, 0);
+    EXPECT_EQ(cirCol.get()->z, 0);
+    EXPECT_FALSE(cirCol.get()->isTrigger);
+    EXPECT_FALSE(cirCol.get()->render);
+}
+
+TEST (AddCircleCollider, InvalidArguments) {
+    vector<string> parameters;
+    parameters.push_back("hello");
+    parameters.push_back("a");
+    parameters.push_back("s");
+    parameters.push_back("d");
+    parameters.push_back("f");
+    parameters.push_back("g");
+
+    Scene scene("Scene", "tmx");
+    Entity e = Engine::getInstance().entities.create();
+    scene.addCircleCollider(parameters, e);
+
+    EXPECT_TRUE(e.has_component<CircleCollider>());
+
+    ComponentHandle<CircleCollider> cirCol = e.component<CircleCollider>();
+
+    EXPECT_EQ(cirCol.get()->radius, 1);
+    EXPECT_EQ(cirCol.get()->x, 0);
+    EXPECT_EQ(cirCol.get()->y, 0);
+    EXPECT_EQ(cirCol.get()->z, 0);
+    EXPECT_FALSE(cirCol.get()->isTrigger);
+    EXPECT_FALSE(cirCol.get()->render);
+}
+
+TEST (AddCircleCollider, MissingArguments) {
+    vector<string> parameters;
+
+    Scene scene("Scene", "tmx");
+    Entity e = Engine::getInstance().entities.create();
+    scene.addCircleCollider(parameters, e);
+
+    EXPECT_TRUE(e.has_component<CircleCollider>());
+
+    ComponentHandle<CircleCollider> cirCol = e.component<CircleCollider>();
+
+    EXPECT_EQ(cirCol.get()->radius, 1);
+    EXPECT_EQ(cirCol.get()->x, 0);
+    EXPECT_EQ(cirCol.get()->y, 0);
+    EXPECT_EQ(cirCol.get()->z, 0);
+    EXPECT_FALSE(cirCol.get()->isTrigger);
+    EXPECT_FALSE(cirCol.get()->render);
+}
+
+TEST (AddCustomScript, ValidPath) {
+    vector<string> parameters;
+    parameters.push_back("../TempleAtonement/src/scripts/player.xml");
+
+    Scene scene("Scene", "tmx");
+    Entity e = Engine::getInstance().entities.create();
+    scene.addCustomScript(parameters, e);
+
+    EXPECT_TRUE(e.has_component<CustomScript>());
+}
+
+TEST (AddCustomScript, InvalidPath) {
+    vector<string> parameters;
+    parameters.push_back("asd");
+
+    Scene scene("Scene", "tmx");
+    Entity e = Engine::getInstance().entities.create();
+    scene.addCustomScript(parameters, e);
+    
+    EXPECT_FALSE(e.has_component<CustomScript>());
+}
+
+TEST (AddCustomScript, MissingPath) {
+    vector<string> parameters;
+
+    Scene scene("Scene", "tmx");
+    Entity e = Engine::getInstance().entities.create();
+    scene.addCustomScript(parameters, e);
+
+    EXPECT_FALSE(e.has_component<CustomScript>());
+}
+
+TEST (AddRigidbody_2D, ValidArguments) {
+    vector<string> parameters;
+    parameters.push_back("2");
+    parameters.push_back("4");
+    parameters.push_back("6");
+    parameters.push_back("7");
+    parameters.push_back("23");
+    parameters.push_back("123");
+    parameters.push_back("67");
+    parameters.push_back("34");
+    parameters.push_back("456");
+    parameters.push_back("456");
+    parameters.push_back("-90");
+    parameters.push_back("34");
+    parameters.push_back("23");
+    parameters.push_back("45");
+
+    Scene scene("Scene", "tmx");
+    Entity e = Engine::getInstance().entities.create();
+    scene.addRigidBody_2D(parameters, e);
+
+    EXPECT_TRUE(e.has_component<Rigidbody_2D>());
+
+    ComponentHandle<Rigidbody_2D> rigidbody2D = e.component<Rigidbody_2D>();
+
+    EXPECT_EQ(rigidbody2D.get()->gravity, 2);
+    EXPECT_EQ(rigidbody2D.get()->mass, 4);
+    EXPECT_EQ(rigidbody2D.get()->linDrag, 6);
+    EXPECT_EQ(rigidbody2D.get()->angDrag, 7);
+    EXPECT_EQ(rigidbody2D.get()->cmX, 23);
+    EXPECT_EQ(rigidbody2D.get()->cmY, 123);
+    EXPECT_EQ(rigidbody2D.get()->cmZ, 67);
+    EXPECT_EQ(rigidbody2D.get()->velocityX, 34);
+    EXPECT_EQ(rigidbody2D.get()->velocityY, 456);
+    EXPECT_EQ(rigidbody2D.get()->angVelocityX, 456);
+    EXPECT_EQ(rigidbody2D.get()->angVelocityY, -90);
+    EXPECT_EQ(rigidbody2D.get()->accelerationX, 34);
+    EXPECT_EQ(rigidbody2D.get()->accelerationY, 23);
+    EXPECT_EQ(rigidbody2D.get()->angAcceleration, 45);
+}
+
+TEST (AddRigidbody_2D, InvalidArguments) {
+    vector<string> parameters;
+    parameters.push_back("a");
+    parameters.push_back("b");
+    parameters.push_back("c");
+    parameters.push_back("d");
+    parameters.push_back("e");
+    parameters.push_back("f");
+    parameters.push_back("g");
+    parameters.push_back("h");
+    parameters.push_back("-i");
+    parameters.push_back("j");
+    parameters.push_back("k");
+    parameters.push_back("");
+    parameters.push_back("m");
+    parameters.push_back("n");
+
+    Scene scene("Scene", "tmx");
+    Entity e = Engine::getInstance().entities.create();
+    scene.addRigidBody_2D(parameters, e);
+
+    EXPECT_TRUE(e.has_component<Rigidbody_2D>());
+
+    ComponentHandle<Rigidbody_2D> rigidbody2D = e.component<Rigidbody_2D>();
+
+    EXPECT_EQ(rigidbody2D.get()->gravity, 1);
+    EXPECT_EQ(rigidbody2D.get()->mass, 0);
+    EXPECT_EQ(rigidbody2D.get()->linDrag, -1);
+    EXPECT_EQ(rigidbody2D.get()->angDrag, 0);
+    EXPECT_EQ(rigidbody2D.get()->cmX, 0);
+    EXPECT_EQ(rigidbody2D.get()->cmY, 0);
+    EXPECT_EQ(rigidbody2D.get()->cmZ, 0);
+    EXPECT_EQ(rigidbody2D.get()->velocityX, 0);
+    EXPECT_EQ(rigidbody2D.get()->velocityY, 0);
+    EXPECT_EQ(rigidbody2D.get()->angVelocityX, 0);
+    EXPECT_EQ(rigidbody2D.get()->angVelocityY, 0);
+    EXPECT_EQ(rigidbody2D.get()->accelerationX, 0);
+    EXPECT_EQ(rigidbody2D.get()->accelerationY, 0);
+    EXPECT_EQ(rigidbody2D.get()->angAcceleration, 0);
+}
+
+TEST (AddRigidbody_2D, MissingArguments) {
+    vector<string> parameters;
+
+    Scene scene("Scene", "tmx");
+    Entity e = Engine::getInstance().entities.create();
+    scene.addRigidBody_2D(parameters, e);
+
+    EXPECT_TRUE(e.has_component<Rigidbody_2D>());
+
+    ComponentHandle<Rigidbody_2D> rigidbody2D = e.component<Rigidbody_2D>();
+
+    EXPECT_EQ(rigidbody2D.get()->gravity, 1);
+    EXPECT_EQ(rigidbody2D.get()->mass, 0);
+    EXPECT_EQ(rigidbody2D.get()->linDrag, -1);
+    EXPECT_EQ(rigidbody2D.get()->angDrag, 0);
+    EXPECT_EQ(rigidbody2D.get()->cmX, 0);
+    EXPECT_EQ(rigidbody2D.get()->cmY, 0);
+    EXPECT_EQ(rigidbody2D.get()->cmZ, 0);
+    EXPECT_EQ(rigidbody2D.get()->velocityX, 0);
+    EXPECT_EQ(rigidbody2D.get()->velocityY, 0);
+    EXPECT_EQ(rigidbody2D.get()->angVelocityX, 0);
+    EXPECT_EQ(rigidbody2D.get()->angVelocityY, 0);
+    EXPECT_EQ(rigidbody2D.get()->accelerationX, 0);
+    EXPECT_EQ(rigidbody2D.get()->accelerationY, 0);
+    EXPECT_EQ(rigidbody2D.get()->angAcceleration, 0);
+}
