93388282622029370ab4f6b5b623dc0aa61fa645
JessicaDevitt <39601330+JessicaDevitt@users.noreply.github.com>
2020-11-22 23:04:19 -0800

    update

---
+++ TempleAtonement/src/maps/dungeon_floor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="dungeon_floor" tilewidth="32" tileheight="32" tilecount="432" columns="24">
+ <image source="../../texture/dungeon_floor.png" width="768" height="576"/>
+</tileset>
